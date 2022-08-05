6fced9344ce780b46e4ca2fc27621586e09c34d1
jiiinang <chjh2425@naver.com>
2022-08-06 00:07:49 +0900

    ApplicationContext 적용

--- src/main/java/com/example/toby/ch1/step1/DaoFactory.java
+++ src/main/java/com/example/toby/ch1/step1/DaoFactory.java
@@ -1,12 +1,18 @@
 package com.example.toby.ch1.step1;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
 
+    @Bean
     public UserDao userDao(){
         return new UserDao(getConnectionMaker());
     }
 
-    private DConnectionMaker getConnectionMaker() {
+    @Bean
+    public DConnectionMaker getConnectionMaker() {
         return new DConnectionMaker();
     }
 
